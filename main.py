e531185cd8165d9741a79672a9c8de4920b3f303
rafal <pacholczykrafal@gmail.com>
2022-09-03 16:33:00 +0200

    First

---
+++ main.py
@@ -0,0 +1,10 @@
+from first_requirement import *
+from second_requirement import *
+"""
+The first requirement
+"""
+OF_amount_verification()
+"""
+The second requirement
+"""
+select_pplus_cyclotron()
